91aab208288e6dcceb22393704440924f90aa94d
mxndyy <amandakj@uci.edu>
2022-04-09 23:49:22 -0700

    .

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,3 +1,4 @@
+--Del. 1
 --The Number of Retiring Employees by Title.
 SELECT e.emp_no,
 	e.first_name, 
@@ -22,10 +23,29 @@ INTO unique_titles
 FROM retirement_titles as rt
 ORDER BY rt.emp_no, rt.to_date DESC;
 
-
 -- The number of employees by their most recent job title who are about to retire.
 SELECT COUNT(ut.titles), ut.titles
 INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.titles
 ORDER BY count DESC;
+
+--Del. 2 
+--Employees Eligible for the Mentorship Program.
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.titles
+INTO mentorship_eligibilty
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no ASC, de.from_date DESC;
